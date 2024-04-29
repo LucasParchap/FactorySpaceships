6fb3f3f4c5b5e840193b21dddaa0514d051e8949
2LP7 <lucas@parchap.com>
2024-04-29 05:26:14 +0200

    Add inventory display functions for spaceships and parts

---
+++ FactorySpaceShips/Models/Spaceship.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace FactorySpaceships.Models;
+
+public class Spaceship
+{
+    private static int _nextId = 0;
+    public int Id { get; private set; }
+    public string Type { get; private set; }
+    public Hull Hull { get; private set; }
+    public Engine Engine { get; private set; }
+    public Wings Wings { get; private set; }
+    public List<Thruster> Thrusters { get; private set; }
+
+    public Spaceship(string type,Hull hull, Engine engine, Wings wings, List<Thruster> thrusters)
+    {
+        Id = ++_nextId;
+        Type = type;
+        Hull = hull;
+        Engine = engine;
+        Wings = wings;
+        Thrusters = thrusters;
+    }
+}
---
+++ FactorySpaceShips/Models/Thruster.cs
@@ -0,0 +1,14 @@
+﻿namespace FactorySpaceships.Models;
+
+public class Thruster : Part
+{
+    private static int _nextSerialNumber;
+    
+    public Thruster(string name): base(name) 
+    {
+    }
+    protected override string GenerateSerialNumber()
+    {
+        return $"T{(++_nextSerialNumber).ToString("D5")}";
+    }
+}
