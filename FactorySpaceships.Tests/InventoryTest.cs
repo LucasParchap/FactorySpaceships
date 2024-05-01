6a6dec2b70b233650cedfe2aff79757d3525f3f2
2LP7 <lucas@parchap.com>
2024-05-01 03:32:58 +0200

    Add unit tests for inventory functionalities

---
+++ FactorySpaceships.Tests/InventoryTest.cs
@@ -0,0 +1,100 @@
+using FactorySpaceships.Models;
+namespace FactorySpaceships.Tests;
+public class InventoryTest
+{
+    static Hull hull_HE1 = new Hull("Hull_HE1");
+    static Engine engine_EE1 = new Engine("Engine_EE1");
+    static Wings wings_WE1 = new Wings("Wings_WE1");
+    static Thruster thruster_TE1 = new Thruster("Thruster_TE1");
+
+    static Hull hull_HS1 = new Hull("Hull_HS1");
+    static Engine engine_ES1 = new Engine("Engine_ES1");
+    static Wings wings_WS1 = new Wings("Wings_WS1");
+    static Thruster thruster_TS1 = new Thruster("Thruster_TS1");
+    static Thruster thruster_TS1_2 = new Thruster("Thruster_TS1");
+    
+    static Hull hull_HC1 = new Hull("Hull_HC1");
+    static Engine engine_EC1 = new Engine("Engine_EC1");
+    static Wings wings_WC1 = new Wings("Wings_WC1");
+    static Thruster thruster_TC1 = new Thruster("Thruster_TC1");
+
+    private Spaceship Explorer =
+        new Spaceship("Explorer", hull_HE1, engine_EE1, wings_WE1, new List<Thruster>() { thruster_TE1 });
+
+    private Spaceship Speeder = new Spaceship("Speeder", hull_HS1, engine_ES1, wings_WS1,
+        new List<Thruster>() { thruster_TS1, thruster_TS1_2 });
+
+    private Spaceship Cargo =
+        new Spaceship("Cargo", hull_HC1, engine_EC1, wings_WC1, new List<Thruster>() { thruster_TC1 });
+
+    private Assembly Assembly1 = new Assembly();
+    private Assembly Assembly2 = new Assembly("Assembly2");
+    
+
+    private Inventory _inventory;
+    
+    [Fact]
+    public void ShouldCorrectlyCountSpaceshipTypes()
+    {
+        _inventory = new Inventory();
+        _inventory.Spaceships.Add(Explorer);
+        _inventory.Spaceships.Add(Speeder);
+        _inventory.Spaceships.Add(Cargo);
+        
+        var expectedCounts = new Dictionary<string, int>
+        {
+            { "Explorer", 1 },
+            { "Speeder", 1 },
+            { "Cargo", 1 }
+        };
+
+        var actualCounts = _inventory.GetSpaceshipTypeCounts();
+
+        Assert.Equal(expectedCounts, actualCounts);
+    }
+    
+    [Fact]
+    public void ShouldCorrectlyCountParts()
+    {
+        _inventory = new Inventory();
+        _inventory.Parts.Add(hull_HE1);
+        _inventory.Parts.Add(hull_HE1);
+        _inventory.Parts.Add(engine_EE1);
+        _inventory.Parts.Add(wings_WE1);
+        _inventory.Parts.Add(thruster_TE1);
+        
+        var expectedCounts = new Dictionary<string, int>
+        {
+            { "Hull_HE1", 2 },
+            { "Engine_EE1", 1 },
+            { "Wings_WE1", 1 },
+            { "Thruster_TE1", 1 }
+        };
+
+        var actualCounts = _inventory.GetPartCounts();
+
+        Assert.Equal(expectedCounts, actualCounts);
+    }
+    
+    [Fact]
+    public void ShouldCorrectlyDisplayAssemblies()
+    {
+        _inventory = new Inventory();
+        
+        Assembly1.AddPart(hull_HE1);
+        Assembly1.AddPart(engine_EE1);
+        
+        Assembly2.AddPart(wings_WE1);
+        Assembly2.AddPart(thruster_TE1);
+
+
+        var expectedDescription1  = "[Hull_HE1, Engine_EE1]";
+        var actualCounts1 = Assembly1.ToString();
+        
+        var expectedDescription2  = "Assembly2";
+        var actualDescription2  = Assembly2.ToString();
+
+        Assert.Equal(expectedDescription1 , actualCounts1);
+        Assert.Equal(expectedDescription2 , actualDescription2);
+    }
+}
