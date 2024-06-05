004dd9a95e15bbbc86ee7b214535990a338be9ed
LucasParchap <lucas@parchap.com>
2024-06-05 04:35:08 +0200

    Update Command Processing interface

---
+++ FactorySpaceShips/Error/CommandLineErrorHandler.cs
@@ -0,0 +1,75 @@
+﻿namespace FactorySpaceships.Error;
+
+public class CommandLineErrorHandler : ErrorHandler
+{
+    private string message; 
+    public override void PrintError()
+    {
+        Console.WriteLine(message);
+    }
+
+    public string GetErrorMessage()
+    {
+        return message;
+    }
+    
+    public bool ValidateArgumentsStructure(string input)
+    {
+        string[] argumentsParts = input.Split(new char[] { ' ' }, 2);
+        if (argumentsParts.Length < 2 || string.IsNullOrWhiteSpace(argumentsParts[1]))
+        {
+            message = "";
+            return true;
+        }
+
+        string[] arguments = argumentsParts[1].Split(',');
+        foreach (string argument in arguments)
+        {
+            string trimmedArgument = argument.Trim();
+            if (string.IsNullOrEmpty(trimmedArgument))
+            {
+                message = "Empty argument detected. Ensure arguments are separated by a single comma without extra spaces.";
+                return false;
+            }
+            
+            string[] parts = trimmedArgument.Split(' ');
+            if (parts.Length != 2 || string.IsNullOrWhiteSpace(parts[0]) || string.IsNullOrWhiteSpace(parts[1]))
+            {
+                message = $"Each argument must consist of exactly two parts: a quantity and a name, separated by one space. Problem found with '{trimmedArgument}'.";
+                return false;
+            }
+            
+        }
+
+        message = "";
+        return true;
+    }
+
+
+    public bool ValidateArguments(string[] arguments)
+    {
+
+        if (arguments != null)
+        {
+            foreach (string arg in arguments)
+            {
+                string[] parts = arg.Split(' ');
+                if (!int.TryParse(arg.Split(' ')[0], out _))
+                {
+                    message = "A numeric value is expected as the first argument.";
+                    return false;
+                }
+                string namePart = parts[1];
+                if (namePart.All(char.IsDigit))
+                {
+                    message = "A string value is expected as the second argument, not just numbers.";
+                    return false;
+                }
+            } 
+        }
+        
+        message = "";
+        return true;
+    }
+    
+}
