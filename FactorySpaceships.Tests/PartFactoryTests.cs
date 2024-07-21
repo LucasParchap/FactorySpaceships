7a63a0c57ae9b1a5e3fad0075f08648b7dd8f164
Mili <muhamed.mili92@gmail.com>
2024-07-21 19:16:54 +0200

    add factory test desgin pattern

---
+++ FactorySpaceships.Tests/PartFactoryTests.cs
@@ -0,0 +1,46 @@
+﻿using FactorySpaceships.Models.Factories;
+
+namespace FactorySpaceships.Tests;
+
+public class PartFactoryTests
+{
+    [Fact]
+    public void HullFactory_ShouldCreateHull()
+    {
+        var hullFactory = new HullFactory();
+        var hull = hullFactory.CreatePart("Hull_HE1");
+
+        Assert.NotNull(hull);
+        Assert.Equal("Hull_HE1", hull.Name);
+    }
+
+    [Fact]
+    public void EngineFactory_ShouldCreateEngine()
+    {
+        var engineFactory = new EngineFactory();
+        var engine = engineFactory.CreatePart("Engine_EE1");
+
+        Assert.NotNull(engine);
+        Assert.Equal("Engine_EE1", engine.Name);
+    }
+
+    [Fact]
+    public void WingsFactory_ShouldCreateWings()
+    {
+        var wingsFactory = new WingsFactory();
+        var wings = wingsFactory.CreatePart("Wings_WE1");
+
+        Assert.NotNull(wings);
+        Assert.Equal("Wings_WE1", wings.Name);
+    }
+
+    [Fact]
+    public void ThrusterFactory_ShouldCreateThruster()
+    {
+        var thrusterFactory = new ThrusterFactory();
+        var thruster = thrusterFactory.CreatePart("Thruster_TE1");
+
+        Assert.NotNull(thruster);
+        Assert.Equal("Thruster_TE1", thruster.Name);
+    }
+}
---
+++ FactorySpaceships.Tests/SpaceshipFactoryTests.cs
@@ -0,0 +1,55 @@
+﻿using FactorySpaceships.Config;
+using FactorySpaceships.Models;
+using FactorySpaceships.Models.Factories;
+using Moq;
+
+
+namespace FactorySpaceships.Tests;
+public class SpaceshipFactoryTests
+{
+    private Mock<IPartFactory<Hull>> mockHullFactory;
+    private Mock<IPartFactory<Engine>> mockEngineFactory;
+    private Mock<IPartFactory<Wings>> mockWingsFactory;
+    private Mock<IPartFactory<Thruster>> mockThrusterFactory;
+    private SpaceshipFactory spaceshipFactory;
+
+    public SpaceshipFactoryTests()
+    {
+        mockHullFactory = new Mock<IPartFactory<Hull>>();
+        mockEngineFactory = new Mock<IPartFactory<Engine>>();
+        mockWingsFactory = new Mock<IPartFactory<Wings>>();
+        mockThrusterFactory = new Mock<IPartFactory<Thruster>>();
+
+        spaceshipFactory = new SpaceshipFactory(mockHullFactory.Object, mockEngineFactory.Object, mockWingsFactory.Object, mockThrusterFactory.Object);
+    }
+
+    [Fact]
+    public void CreateSpaceshipFromConfig_ShouldCreateCompleteSpaceship()
+    {
+        var config = new SpaceshipConfig.SpaceshipData
+        {
+            Type = "Explorer",
+            Parts = new Dictionary<string, int>
+            {
+                { "Hull_HE1", 1 },
+                { "Engine_EE1", 1 },
+                { "Wings_WE1", 1 },
+                { "Thruster_TE1", 1 }
+            }
+        };
+
+        mockHullFactory.Setup(f => f.CreatePart("Hull_HE1")).Returns(new Hull("Hull_HE1"));
+        mockEngineFactory.Setup(f => f.CreatePart("Engine_EE1")).Returns(new Engine("Engine_EE1"));
+        mockWingsFactory.Setup(f => f.CreatePart("Wings_WE1")).Returns(new Wings("Wings_WE1"));
+        mockThrusterFactory.Setup(f => f.CreatePart(It.IsAny<string>())).Returns(new Thruster("Thruster_TE1"));
+
+        var spaceship = spaceshipFactory.CreateSpaceshipFromConfig(config);
+
+        Assert.NotNull(spaceship);
+        Assert.Equal("Explorer", spaceship.Type);
+        Assert.NotNull(spaceship.Hull);
+        Assert.NotNull(spaceship.Engine);
+        Assert.NotNull(spaceship.Wings);
+        Assert.Single(spaceship.Thrusters);
+    }
+}
